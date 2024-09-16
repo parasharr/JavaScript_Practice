8c6883ca04952cc0dc78017536fe837aeb1ae0a7
PranjeetGoswami <prgoswami2020@gmail.com>
2024-09-16 19:11:53 +0530

    add to repo

--- datatypes.js
+++ datatypes.js
@@ -1,19 +1,49 @@
-let name = "Pranjeet"
-let age = 19
-let isLoggedIn = true
-let state;
+// let name = "Pranjeet"
+// let age = 19
+// let isLoggedIn = true
+// let state;
 
-//primitve datatypes
+// //primitve datatypes
 
-//number => 2 to power 53 
-//bigInt
-//String => ""
-//boolean => true/false
-//null => is a standalone value
-//undefined
-//symbol => unique
+// //number => 2 to power 53 
+// //bigInt
+// //String => ""
+// //boolean => true/false
+// //null => is a standalone value
+// //undefined
+// //symbol => unique
 
-//objects
+// //objects
 
-console.log(typeof undefined); //undefined
-console.log(typeof null); //object
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); //object
+
+//conversion
+// let score = 33;
+// let score2 = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score2));
+
+// let valueInNumber = Number(score2);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false;
+//""=>false
+//"pranjeet => true"
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
