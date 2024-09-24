5477ac3d90c996f2184e44c7c1f8a70fcf8f2d68
PranjeetGoswami <prgoswami2020@gmail.com>
2024-09-24 20:10:46 +0530

    add to repo

---
+++ functions.js
@@ -0,0 +1,18 @@
+// function sayMyName(){
+//     console.log("P")
+//     console.log("R")
+//     console.log("A")
+//     console.log("N")
+//     console.log("J")
+//     console.log("E")
+//     console.log("E")
+//     console.log("T")
+// }
+
+// sayMyName()
+
+function addTwoNums(num1, num2) {
+    console.log(num1 + num2);
+}
+
+addTwoNums(3,4);
