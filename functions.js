607841ab9c431f194c058ff815dff93a44d504d5
PranjeetGoswami <prgoswami2020@gmail.com>
2024-09-25 19:53:15 +0530

    add to repo

--- functions.js
+++ functions.js
@@ -11,8 +11,43 @@
 
 // sayMyName()
 
-function addTwoNums(num1, num2) {
-    console.log(num1 + num2);
+// function addTwoNums(num1, num2) {//parameters
+//     console.log(num1 + num2);
+// }
+// const result = addTwoNums(3,4)//output 7 from function
+// console.log("Result", result) //the result will be undefined 
+
+function addTwoNums(num1, num2) {//parameters
+    let result = num1 + num2
+    return result
+    // console.log("Hi") //after result the functon won't work
 }
 
-addTwoNums(3,4);
+const result = addTwoNums(3,4)
+// console.log("Result", result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`;
+}
+
+// console.log(loginUserMessage("Pranjeet"))
+
+
+
+
+function calculateCartPrice(...num1){//... rest operator
+    return num1
+}
+
+// console.log(calculateCartPrice(200,4000,5000));/
+
+const user ={
+    username: "Pranjeet",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
