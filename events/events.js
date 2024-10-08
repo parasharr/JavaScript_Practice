4419228fc23831641b06515c5e79ebdb83654ec0
PranjeetGoswami <prgoswami2020@gmail.com>
2024-10-08 19:00:15 +0530

    add to repo

---
+++ events/events.js
@@ -0,0 +1,33 @@
+// events are run sequentially in javascript, But some browser events are not work in a sequencial manner
+
+// const img = document.querySelector('#images')
+const owl = document.querySelector('#owl')
+
+// img.addEventListener('click', function(e){
+//     // console.log("clicked inside the ul");
+    
+// }) // flase parameter is deafult false so we don't need to write false  (event bubbling and event capturing)
+
+owl.addEventListener('click', function(e){
+    // console.log("owl clicked");
+})
+
+//true and false will depend on our use cases
+
+//types
+//timeStamps
+//default Prevented
+//target
+//toElement
+//sourceElement
+//currentTarget
+//Client position
+//screen position
+
+const img = document.querySelector('#images')
+
+img.addEventListener('click', function(e){
+    console.log(e.target.parentNode);
+    let removeIt = e.target.parentNode;
+    removeIt.remove();
+})
